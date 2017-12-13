cf5d0f8b4e3a7c703d2de82f37d1c17bdbee4241
chanmi168 <chanmi168@gmail.com>
2017-12-13 03:39:16 -0500

    last commit tonight

--- Michael/code/main.py
+++ Michael/code/main.py
@@ -1,14 +1,23 @@
+#%%
+
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
-# plt.show()
-
+plt.show()
+#%%
 bbb = 1
 print('I am : %d' % bbb)     
 
 
 
 import numpy as np
-
+import pandas as pd
 aaa = np.asarray([0,1,2,3,4])
-print(aaa)
+
+data = {'Country': ['Belgium', 'India', 'Brazil'],
+'Capital': ['Brussels', 'New Delhi', 'Brasília'],
+'Population': [11190846, 1303171035, 207847528]}
+df = pd.DataFrame(data,
+columns=['Country', 'Capital', 'Population'])
+
+print(df)
