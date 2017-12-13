25cb57a3b03f8227452c15f74cdcc5aa54560842
chanmi168 <chanmi168@gmail.com>
2017-12-13 02:28:54 -0500

    1st commit on VS code

---
+++ Michael/code/main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
+bbb = 1
+print('I am : %d' % bbb)     
+
+
+
+import numpy as np
+
+aaa = np.asarray([0,1,2,3,4])
+print(aaa)
