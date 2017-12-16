5795af2459588932a9ab31947aa62184b16e164d
chanmi168 <chanmi168@gmail.com>
2017-12-16 03:51:09 -0500

    starting project code implementation

--- Michael/code/main.py
+++ Michael/code/main.py
@@ -1,23 +1,15 @@
-#%%
+# Everything
 
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
-plt.show()
-#%%
-bbb = 1
-print('I am : %d' % bbb)     
+
+# %% Loading data and labels
+
+# loop through all folders, import all files in a folder 
 
 
 
-import numpy as np
-import pandas as pd
-aaa = np.asarray([0,1,2,3,4])
 
-data = {'Country': ['Belgium', 'India', 'Brazil'],
-'Capital': ['Brussels', 'New Delhi', 'Brasília'],
-'Population': [11190846, 1303171035, 207847528]}
-df = pd.DataFrame(data,
-columns=['Country', 'Capital', 'Population'])
+# %% Preporcess data: 
+#   -> downsample   -> fragmentation    -> extract DWT coeff 
+#   -> compute energy index
 
-print(df)
+# %% LSTM implementation
---
+++ Michael/code/modules/dataParser.py
@@ -0,0 +1,5 @@
+
+# This file will help handle data preprocessing
+
+
+class MP3loader ():
