fe578f547bcd810e116952c5d8e5682102b08a00
chanmi168 <chanmi168@gmail.com>
2017-12-17 03:13:10 -0500

    Added LstmWavelet.py

--- Michael/code/main.py
+++ Michael/code/main.py
@@ -42,4 +42,49 @@ plt.show()
 #   -> downsample   -> fragmentation    -> extract DWT coeff 
 #   -> compute energy index
 
-# %% LSTM implementation
+# %% Run using LSTM
+
+WAVELET_DIM = 64
+HIDDEN_DIM = 512
+OUTPUT_DIM = 300 // # of speakers
+model = LstmWavelet(HIDDEN_DIM, WAVELET_DIM, OUTPUT_DIM)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+ # pre-run score
+
+for _,wavelefCoeff in get_waveletCoeff(file):
+    tensor = autograd.Variable(torch.from_numpy(wavelefCoeff))
+    tag_scores = model(tensor)
+    print(tag_scores)
+    break
+
+# train for 6 epoches (70%)
+for epoch in range(6):
+    for key,wavelefCoeff in get_waveletCoeff(trainFiles): # trainFiles contain all wavelet coeff 
+                                                            # for all training dataset
+        #if key not in key2num:
+        #    continue
+        tensor = autograd.Variable(torch.from_numpy(wavelefCoeff))
+        tensor = tensor.cuda() # use gpu
+        targets = autograd.Variable(torch.from_numpy())
+        targets = targets.cuda()
+
+        model.zero_grad()
+        model.hidden = model.init_hidden()
+
+        # forward pass
+        tag_scores = model(tensor)
+
+        # backprop
+        loss = loss_function(tag_scores, targets)
+        loss.backward()
+        optimizer.step()
+
+# post-run score (30%)
+for _,wavelefCoeff in get_waveletCoeff(testFiles): # testFiles contain all wavelet coeff 
+                                                            # for all testing dataset
+    tensor = torch.from_numpy(wavelefCoeff)
+    tag_scores = model(tensor)
+    print(tag_scores)
+    break
+
