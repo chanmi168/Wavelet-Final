10eaab07c8b7e50f476f85281fb21d1178385812
chanmi168 <chanmi168@gmail.com>
2017-12-17 04:36:38 -0500

    added wav2wpc.py (compute wavelet coeff energy)

--- Michael/code/dataParser.py
+++ Michael/code/dataParser.py
@@ -1,10 +1,37 @@
 
 # This file will help handle data preprocessing
-# from mutagen.mp3 import MP3
 import scipy.io.wavfile as wavefile
+import matplotlib.pyplot as plt
+import numpy as np
 
+import pywt
 
 def wavLoader(filename='example.wav'):
-    rate, data = wavefile.read(filename)
-    
+    rate, data = wavefile.read(filename) 
     return rate, data
+
+def wav2wpc(data):
+    wavelet = 'db2'
+    level = 4
+    order = "freq"  # other option is "normal"
+    interpolation = 'nearest'
+    cmap = plt.cm.cool
+    # Construct wavelet packet
+    wp = pywt.WaveletPacket(data, wavelet, 'symmetric', maxlevel=level)
+    nodes = wp.get_level(level, order=order)
+    labels = [n.path for n in nodes]
+    waveletCoeffs = np.array([n.data for n in nodes], 'd')
+    waveletCoeffs = abs(waveletCoeffs)
+    # shannonEntropy = getShannonEntropy(waveletCoeffs)
+    energyIndex = getEnergyIndices(waveletCoeffs)
+    return energyIndex
+
+def getEnergyIndices(waveletCoeffs):
+    (coeffN, _) = waveletCoeffs.shape
+    energyIndices = []
+    # energyIndices = np.asarray(energyIndices)
+    energyIndices = np.zeros((coeffN,1))
+    for i, row in enumerate(waveletCoeffs):
+        # aaa = np.dot(row, row)/len(row)
+        energyIndices[i] = np.dot(row, row)/len(row)
+    return energyIndices
--- Michael/code/main.py
+++ Michael/code/main.py
@@ -1,6 +1,6 @@
 # %% Import modules
 
-from dataParser import wavLoader
+from dataParser import wavLoader, wav2wpc
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -25,10 +25,10 @@ fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
 ax2.plot(bbb)
 
-plt.show()
+# plt.show()
 
-
-
+# Calculate wavelet coeff energy
+waveletCoeffs = wav2wpc(bbb)
 
 
 
@@ -44,47 +44,47 @@ plt.show()
 
 # %% Run using LSTM
 
-WAVELET_DIM = 64
-HIDDEN_DIM = 512
-OUTPUT_DIM = 300 // # of speakers
-model = LstmWavelet(HIDDEN_DIM, WAVELET_DIM, OUTPUT_DIM)
-loss_function = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.1)
- # pre-run score
+# WAVELET_DIM = 64
+# HIDDEN_DIM = 512
+# OUTPUT_DIM = 300 // # of speakers
+# model = LstmWavelet(HIDDEN_DIM, WAVELET_DIM, OUTPUT_DIM)
+# loss_function = nn.NLLLoss()
+# optimizer = optim.SGD(model.parameters(), lr=0.1)
+#  # pre-run score
 
-for _,wavelefCoeff in get_waveletCoeff(file):
-    tensor = autograd.Variable(torch.from_numpy(wavelefCoeff))
-    tag_scores = model(tensor)
-    print(tag_scores)
-    break
+# for _,wavelefCoeff in get_waveletCoeff(file):
+#     tensor = autograd.Variable(torch.from_numpy(wavelefCoeff))
+#     tag_scores = model(tensor)
+#     print(tag_scores)
+#     break
 
-# train for 6 epoches (70%)
-for epoch in range(6):
-    for key,wavelefCoeff in get_waveletCoeff(trainFiles): # trainFiles contain all wavelet coeff 
-                                                            # for all training dataset
-        #if key not in key2num:
-        #    continue
-        tensor = autograd.Variable(torch.from_numpy(wavelefCoeff))
-        tensor = tensor.cuda() # use gpu
-        targets = autograd.Variable(torch.from_numpy())
-        targets = targets.cuda()
+# # train for 6 epoches (70%)
+# for epoch in range(6):
+#     for key,wavelefCoeff in get_waveletCoeff(trainFiles): # trainFiles contain all wavelet coeff 
+#                                                             # for all training dataset
+#         #if key not in key2num:
+#         #    continue
+#         tensor = autograd.Variable(torch.from_numpy(wavelefCoeff))
+#         tensor = tensor.cuda() # use gpu
+#         targets = autograd.Variable(torch.from_numpy())
+#         targets = targets.cuda()
 
-        model.zero_grad()
-        model.hidden = model.init_hidden()
+#         model.zero_grad()
+#         model.hidden = model.init_hidden()
 
-        # forward pass
-        tag_scores = model(tensor)
+#         # forward pass
+#         tag_scores = model(tensor)
 
-        # backprop
-        loss = loss_function(tag_scores, targets)
-        loss.backward()
-        optimizer.step()
+#         # backprop
+#         loss = loss_function(tag_scores, targets)
+#         loss.backward()
+#         optimizer.step()
 
-# post-run score (30%)
-for _,wavelefCoeff in get_waveletCoeff(testFiles): # testFiles contain all wavelet coeff 
-                                                            # for all testing dataset
-    tensor = torch.from_numpy(wavelefCoeff)
-    tag_scores = model(tensor)
-    print(tag_scores)
-    break
+# # post-run score (30%)
+# for _,wavelefCoeff in get_waveletCoeff(testFiles): # testFiles contain all wavelet coeff 
+#                                                             # for all testing dataset
+#     tensor = torch.from_numpy(wavelefCoeff)
+#     tag_scores = model(tensor)
+#     print(tag_scores)
+#     break
 
